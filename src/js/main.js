388f98551d3f1c2dafc14c4a08ee92eea129a635
chuoi2taps <thiennnph25438@fpt.edu.vn>
2022-06-15 20:29:25 +0700

    update

--- src/js/main.js
+++ src/js/main.js
@@ -13,14 +13,14 @@ function showProducts(products) {
         const product = products[i];
         result += `
         <div class="column">
-        <a href=""><img src="${product.img}" class="rounded-3xl" alt="" /></a>
+        <a href="/detail.html?id=${product.id}"><img src="${product.img}" class="rounded-3xl" alt="" /></a>
         <h3 class="text-[#F54748] pt-5 font-bold">
-            <a href="">${product.name}</a>
+            <a href="/detail.html?id=${product.id}">${product.name}</a>
         </h3>
         <div class="flex pt-4 items-start">
-            <span class=""><img src="img/Vector.png" alt="" /></span>
+            <span class=""><img src="${product.icon}" alt="" /></span>
             <h3 class="pl-1">30 Minutes</h3>
-            <span><img src="img/ForkKnife.png" alt="" class="pl-5" /></span>
+            <span><img src="${product.icon}" alt="" class="pl-5" /></span>
             <h3 class="pl-1">Snack</h3>
         </div>
     </div>
